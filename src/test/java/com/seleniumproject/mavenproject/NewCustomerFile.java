a7964911fa2f0ed383ec1096719dd548395e0c9a
dinesh <dinesh.trustgrid@gmail.com>
2023-07-27 13:26:23 +0530

    Newcustomerfile added and Newfileadded renamed

---
+++ src/test/java/com/seleniumproject/mavenproject/NewCustomerFile.java
@@ -0,0 +1,5 @@
+package com.seleniumproject.mavenproject;
+
+public class NewCustomerFile {
+
+}
---
+++ src/test/java/com/seleniumproject/mavenproject/Newfileadded.java
@@ -0,0 +1,7 @@
+package com.seleniumproject.mavenproject;
+
+// New File Added by other Person in our team.
+//
+
+public class Newfileadded{
+}
